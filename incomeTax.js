59d8d0b8f6dc29411831cab4655e7257d59e3917
zoeibanez <bzvibanez@addu.edu.ph>
2024-12-06 17:41:36 +0800

    Update incomeTax.js

--- incomeTax.js
+++ incomeTax.js
@@ -36,3 +36,8 @@ function computeTax() {
     // Set the computed tax to the output field
     document.getElementById('totaltax').value = tax.toFixed(2); // Rounded to 2 decimal places
 }
+// Toggle the menu visibility when burger icon is clicked
+function toggleMenu() {
+    const nav = document.querySelector('nav');
+    nav.classList.toggle('active');
+}
